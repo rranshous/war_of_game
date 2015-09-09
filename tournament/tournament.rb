0b7539e5abffbdcd2fc304e5de334921f2e9b3d7
Robby Ranshous <rranshous@gmail.com>
2015-09-09 10:29:02 -0400

    dont print board

--- tournament/tournament.rb
+++ tournament/tournament.rb
@@ -34,7 +34,7 @@ class Tournament
       begin
         Timeout::timeout(10) do # WHY CAN I GET TIMEOUTS ON TICKS?!
           sim.tick
-          sim.print_board
+          #sim.print_board
           #sleep 0.5
         end
       rescue Timeout::Error
