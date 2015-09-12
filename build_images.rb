baa428ca11d0d1fe81ba9a55de855426c1d8b311
Robby Ranshous <rranshous@gmail.com>
2015-09-12 03:26:03 -0400

    moving docker containres to hub

---
+++ build_images.rb
@@ -0,0 +1,21 @@
+require 'thread'
+threads = []
+filter = ARGV.to_a
+Dir["Dockerfile.*"].each do |file|
+  threads << Thread.new(file) do |f|
+    image_name = "rranshous/wog_#{f.split('.').last}"
+    if filter.length > 0 && !filter.include?(image_name)
+      next
+    end
+    cmd = "docker pull #{image_name}"
+    puts "CMD: #{cmd}"
+    system(cmd)
+    cmd = "docker build -f #{f} -t #{image_name} ."
+    puts "CMD: #{cmd}"
+    system(cmd)
+    cmd = "docker push #{image_name}"
+    puts "CMD: #{cmd}"
+    system(cmd)
+  end
+end
+threads.map(&:join)
