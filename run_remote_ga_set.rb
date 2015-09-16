e9f4891e3c675809c549982fd8697102a7e084b1
Robby Ranshous <rranshous@gmail.com>
2015-09-16 00:00:49 -0400

    update readme

--- run_remote_ga_set.rb
+++ run_remote_ga_set.rb
@@ -1,7 +1,7 @@
 set_name = ARGV.shift || 'default'
 puts "building test set #{set_name}"
 system('docker build -t war_of_game_ga .')
-[ [100, 30], [50, 40] ].each do |pop, gens|
+[ [100, 30] ].each do |pop, gens|
   container_name = "wog_ga_#{pop}x#{gens}_#{set_name}"
   puts "rm old container"
   cmd = "docker rm #{container_name}"
